8ae20bd0b91df3d4be9e11d847dc6b16b8c54255
00018169 <00018169@wiut.uz>
2025-11-23 00:44:56 +0500

    third commit

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4umZN7TqdHBuloE3ru1txq5=d3ew6i32HbDXWjYch'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
